018be3cb10e8701ce3d21631bed7e0218c038e39
laila ouhamou <laila.ouhamou99@gmail.com>
2022-10-31 15:57:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+
+import { useCallback, useEffect, useRef, useState } from 'react';
+import Webcam from 'react-webcam';
 import './App.css';
 
 function App() {
+  const [image,setImage]=useState(null)
+  const webCamRef=useRef(null)
+  const [deviceId, setDeviceId]=useState({})
+  const [devices, setDevices]=useState([])
+  const handleDevices = useCallback((mediaDevices) => {
+      setDevices(mediaDevices.filter(({kind})=> kind==="videoinput"))
+  }
+  ,[setDevices])
+  useEffect(() => {
+    navigator.mediaDevices.enumerateDevices().then(handleDevices)
+  }, [handleDevices])
+  
+  
+  const takePicture = useCallback(() => {
+      const imageSrc=webCamRef.current.getScreenshot()
+      setImage(imageSrc)
+  },[webCamRef])
+  const videoConstraints = {
+    facingMode: { exact: "environment" }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="section">
+                <div className="sectionContainer ">
+                        <div className="sectionContent">
+                            {
+                                devices.map((device,key)=>
+                                
+                                {
+                                  console.log("device",device)
+                                  return(
+                                  (
+                                    <div key={key} className='cameraItem'>
+                                        <Webcam
+                                        ref={webCamRef}
+                                        screenshotFormat="image/jpeg"
+                                        audio={false}
+                                       videoConstraints = {videoConstraints}
+                                        />
+                                        {"device "+device.label || 'device '+key + 1}
+                                    </div>
+                                )
+                                )}
+                                )
+                            }
+                         <div 
+                        onClick={takePicture}
+                         className="cursor-pointer webCamCaptureBtn translate-x-[-50%] left-[50%] bottom-[10px] w-[40px] h-[40px] border-2 border-[black] rounded-full absolute ">
+                        </div>
+                        </div>
+                        <img className='w-[350px] h-[200px] object-cover' src={image} alt="" />
+                </div>
+            </section>
     </div>
   );
 }
